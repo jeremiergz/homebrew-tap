fb1e84c4571dc2c4fe325333fc6bfa849d05073a
Jeremie Rodriguez <contact@jeremierodriguez.com>
2020-10-06 00:51:16 +0200

    chore(nas-cli): update to v20.10.1

--- Formula/nas-cli.rb
+++ Formula/nas-cli.rb
@@ -1,12 +1,12 @@
 class NasCli < Formula
   desc "CLI application for managing my NAS."
   homepage "https://github.com/jeremiergz/nas-cli"
-  url "https://github.com/jeremiergz/nas-cli/releases/download/20.10.0/nas-cli-darwin-amd64"
+  url "https://github.com/jeremiergz/nas-cli/releases/download/20.10.1/nas-cli-darwin-amd64"
   head "https://github.com/jeremiergz/nas-cli.git"
-  sha256 "d29fa4ce01116f08ad88cef3c024f32b7bbb41a8a030b0b0c981cbde2156b1e3"
+  sha256 "b5a29a5e658a1926451592a70add3ea1c602a6392070e0a60ee37eaebee35b78"
   license "Apache-2.0"
 
-  version "20.10.0"
+  version "20.10.1"
   conflicts_with "nas-cli"
   bottle :unneeded
 
