37d90c9104779078a6a9e8010b5327b7888ab630
Jeremie Rodriguez <jeremie.rodriguez@kobionic.com>
2021-04-07 17:24:36 +0200

    chore(nas-cli): update to v21.04.0

--- Formula/nas-cli.rb
+++ Formula/nas-cli.rb
@@ -1,12 +1,12 @@
 class NasCli < Formula
   desc "CLI application for managing my NAS."
   homepage "https://github.com/jeremiergz/nas-cli"
-  url "https://github.com/jeremiergz/nas-cli/releases/download/21.01.2/nas-cli-darwin-amd64"
+  url "https://github.com/jeremiergz/nas-cli/releases/download/21.04.0/nas-cli-darwin-amd64"
   head "https://github.com/jeremiergz/nas-cli.git"
-  sha256 "768d838267445f7112ac679376dc36c145c565b912dd36252de738592dd2a45a"
+  sha256 "2b3bfe002a9612314a60021dfd47677be95da7e5f42d2cbc9990aaa0c6f8cee5"
   license "Apache-2.0"
 
-  version "21.01.2"
+  version "21.04.0"
   conflicts_with "nas-cli"
   bottle :unneeded
 
